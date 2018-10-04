2461977e9646870278feae238adae1450ccfa753
Rubilia <Rubiliam2003@gmail.com>
2018-10-04 23:42:11 +0300

    working version

--- src/main/java/ImageDataFetcher.java
+++ src/main/java/ImageDataFetcher.java
@@ -5,7 +5,6 @@ import org.nd4j.linalg.factory.Nd4j;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.image.PixelGrabber;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -14,7 +13,7 @@ import java.util.List;
 
 public class ImageDataFetcher extends BaseDataFetcher {
     private int height, width;
-    private List<double[][]> inputs;
+    private List<double[][][]> inputs;
     private List<double[]> outputs;
     public ImageDataFetcher(int height, int width, int classesAmount){
         this.cursor = 0;
@@ -41,13 +40,13 @@ public class ImageDataFetcher extends BaseDataFetcher {
     }
 
     public void addImageData(List<BufferedImage> input, List<double[]> output) {
-        List<double[][]> imgData = convertData(input);
+        List<double[][][]> imgData = convertData(input);
         List<Integer> wrongIndexes = new ArrayList<>();
         for (int i = 0; i < imgData.size(); i++) {
             if (imgData.get(i) == null)
                 wrongIndexes.add(i);
         }
-        List<double[][]> newInputs = new ArrayList<>();
+        List<double[][][]> newInputs = new ArrayList<>();
         List<double[]> newOuts = new ArrayList<>();
         for (int i = 0; i < input.size(); i++) {
             if (wrongIndexes.contains(i)){continue;}
@@ -56,17 +55,17 @@ public class ImageDataFetcher extends BaseDataFetcher {
         }
         inputs.addAll(newInputs);
         outputs.addAll(newOuts);
-        dataStorage<List<double[][]>, List<double[]>> mixedData = mixData(inputs, outputs);
+        dataStorage<List<double[][][]>, List<double[]>> mixedData = mixData(inputs, outputs);
         inputs = mixedData.getKey();
         outputs = mixedData.getValue();
         this.totalExamples = inputs.size();
     }
 
-    private dataStorage<List<double[][]>, List<double[]>> mixData(List<double[][]> inps, List<double[]> outs){
+    private dataStorage<List<double[][][]>, List<double[]>> mixData(List<double[][][]> inps, List<double[]> outs){
         List<Integer> indexes =new ArrayList<>();
         for (int i = 0; i < inps.size(); i++) { indexes.add(i); }
         Collections.shuffle(indexes);
-        List<double[][]> mixedInps = new ArrayList<>();
+        List<double[][][]> mixedInps = new ArrayList<>();
         List<double[]> mixedOuts = new ArrayList<>();
         for(int index: indexes){
             mixedInps.add(inps.get(index).clone());
@@ -75,28 +74,30 @@ public class ImageDataFetcher extends BaseDataFetcher {
         return new dataStorage<>(mixedInps, mixedOuts);
     }
 
-    public List<double[][]> convertData(List<BufferedImage> input) {
-        List<double[][]> ret = new ArrayList<>();
+    public List<double[][][]> convertData(List<BufferedImage> input) {
+        List<double[][][]> ret = new ArrayList<>();
         for (int i = 0; i < input.size(); i++) {
             ret.add(extractImage(input.get(i)));
         }
         return ret;
     }
 
-    public double[][] extractImage(BufferedImage image) {
+    public double[][][] extractImage(BufferedImage image) {
         try {
             BufferedImage dimg = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
             Graphics2D g = dimg.createGraphics();
             g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
             g.drawImage(image, 0, 0, width, height, 0, 0, image.getWidth(), image.getHeight(), null);
             g.dispose();
-            int[] prePixels = new int[height*width];
-            PixelGrabber pgb = new PixelGrabber(dimg, 0, 0, width, height, prePixels, 0, width);
-            pgb.grabPixels();
-            double[][] pixels = new double[height][width];
+            int[] prePixels = dimg.getRGB(0, 0, width, height, null, 0, width);
+
+            double[][][] pixels = new double[3][height][width];
             int counter = 0;
             for(int item: prePixels){
-                pixels[counter%width][(counter-counter%width)/width] = -8* Math.pow(10, -8)*(double)item-1.0;
+                Color a =new Color(item);
+                pixels[0][counter%width][(counter-counter%width)/width] = a.getRed()/3-85;
+                pixels[1][counter%width][(counter-counter%width)/width] = a.getGreen()/3-85;
+                pixels[2][counter%width][(counter-counter%width)/width] = a.getBlue()/3-85;
                 counter++;
             }
             return pixels;
@@ -110,10 +111,10 @@ public class ImageDataFetcher extends BaseDataFetcher {
             throw new IllegalStateException("Unable to get more; there are no more images");
         }
         if (amount>this.totalExamples+cursor){amount = this.totalExamples-cursor;}
-        double[][][][] inps = new double[amount][1][height][width];
+        double[][][][] inps = new double[amount][3][height][width];
         double[][] outs = new double[amount][this.numOutcomes];
         for (int i = 0; i < amount && this.hasMore(); i++) {
-            inps[i] = new double[][][]{inputs.get(this.cursor).clone()};
+            inps[i] = inputs.get(this.cursor).clone();
             outs[i] = outputs.get(this.cursor).clone();
             this.cursor++;
         }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import org.deeplearning4j.datasets.fetchers.MnistDataFetcher;
+import org.deeplearning4j.datasets.iterator.impl.MnistDataSetIterator;
 import org.deeplearning4j.eval.Evaluation;
 import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
 import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
@@ -19,143 +19,101 @@ import org.deeplearning4j.zoo.model.VGG16;
 import org.nd4j.jita.conf.CudaEnvironment;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.cpu.nativecpu.NDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
 import org.nd4j.linalg.learning.config.Nesterovs;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 /*Порядок классов:
-    0 - car
-    1 - flower
-    2 - airplnae
-
+    0 - cat
+    1 - dog
  */
 
 public class Main {
-    private static int picturesPerIteration = 180;
-    private static int iterationsAmount = 3;
-    private static final double correctValue = 0.8, wrongValue = 0.1;
-    private static int car_correct = 500;
-    private static int flower_correct = 840;
-    private static int airplane_correct = 530;
-    static List<File> carFiles, flowerFiles, airplaneFiles;
+    private static int picturesPerIteration = 80;
+    private static int iterationsAmount = 1;
+    private static final double correctValue = 0.9, wrongValue = 0.1;
+    static List<File> catFiels, dogFiels;
     private static final Logger log = LoggerFactory.getLogger(Main.class);
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.setProperty("org.bytedeco.javacpp.maxphysicalbytes", "0");
         System.setProperty("org.bytedeco.javacpp.maxbytes", "0");
         CudaEnvironment.getInstance().getConfiguration().allowMultiGPU(true).setMaximumDeviceCache(2L * 1024L * 1024L * 1024L).allowCrossDeviceAccess(true);
-        carFiles = getFiles("C:\\Users\\Rubil\\Documents\\NeuralNetworkProjects\\images\\car\\");
-        flowerFiles = getFiles("C:\\Users\\Rubil\\Documents\\NeuralNetworkProjects\\images\\flower\\");
-        airplaneFiles = getFiles("C:\\Users\\Rubil\\Documents\\NeuralNetworkProjects\\images\\airplane\\");
-        int nChannels = 1;
-        int outputNum = 3;
-        int batchSize = 128;
-        int nEpochs = 10;
+        getFiles("C:\\Users\\Rubil\\Documents\\NeuralNetworkProjects\\Cat_Dog\\train\\");
+        int nChannels = 3;
+        int outputNum = 2;
+        int batchSize = 3;
+        int nEpochs = 100;
         int inputHeight = 160, inputWidth  = 160;
         int seed = (new Random()).nextInt();
-        InceptionResNetV1 zooModel = InceptionResNetV1.builder()
-                .numClasses(outputNum)
-                .seed(seed)
-                .inputShape(new int[]{nChannels, inputHeight, inputWidth})
-                .build();
-        ComputationGraph model = zooModel.init();
-        model.init();
+        log.info("Building a model....");
+        ZooModel inception = InceptionResNetV1.builder().numClasses(2).seed(seed).build();
+        inception.setInputShape(new int[][]{{nChannels, inputHeight, inputWidth}, {batchSize}});
+        ComputationGraph model = inception.init();
+        model.setListeners(new PerformanceListener(1, true));
+        log.info("Loading training data....");
+        DataSetIterator Train =setImagesTrain(new ImageDataSetIterator(batchSize, inputHeight, inputWidth, outputNum), 0, 1200);
+        DataSetIterator mnistTrain = new MnistDataSetIterator(batchSize,true,12345);
+        INDArray mnistData = mnistTrain.next().getFeatures(), data = Train.next().getFeatures();
         System.out.println("Training model....");
-        model.setListeners(new ScoreIterationListener(100));
         long timeX = System.currentTimeMillis();
-        //MultipleEpochsIterator mnistMultiEpochIterator = new MultipleEpochsIterator(nEpochs, mnistTrain);
         for( int i=0; i<nEpochs; i++ ) {
             System.out.println("new Epoch " + i);
             long time1 = System.currentTimeMillis();
             ParallelWrapper wrapper = new ParallelWrapper.Builder(model)
-                    .prefetchBuffer(128)
-                    .workers(3)
-                    .trainingMode(ParallelWrapper.TrainingMode.SHARED_GRADIENTS)
+                    .prefetchBuffer(batchSize)
+                    .workers(4)
                     .averagingFrequency(2)
                     .reportScoreAfterAveraging(true)
                     .build();
             for (int j = 0; j < iterationsAmount; j++) {
-                DataSetIterator Train =setImagesTrain(new ImageDataSetIterator(batchSize, inputHeight, inputWidth, 3), j);
                 wrapper.fit(Train);
             }
             long time2 = System.currentTimeMillis();
             System.out.printf("*** Completed epoch {}, time: {} ***", i, (time2 - time1)/1000 + " sec");
         }
         long timeY = System.currentTimeMillis();
-
         System.out.printf("*** Training complete, time: {} ***", (timeY - timeX));
-        DataSetIterator Test = new ImageDataSetIterator(batchSize, inputHeight, inputWidth, 3);
-        Test = setImagesTest((ImageDataSetIterator) Test);
+        log.info("Getting data for test");
+        DataSetIterator Test = setImagesTrain(new ImageDataSetIterator(batchSize, inputHeight, inputWidth, outputNum), 10000, 10500);
         log.info("Evaluate model....");
         Evaluation eval = model.evaluate(Test);
         log.info(eval.stats());
-        Test.reset();
-
         System.out.println("****************Example finished********************");
     }
 
-    static ArrayList<File> getFiles(String path){
-        ArrayList<File> ret = new ArrayList<>();
+    static void getFiles(String path){
+        catFiels = new ArrayList<>();
+        dogFiels = new ArrayList<>();
         File file = new File(path);
         for(File f: file.listFiles()){
-            if (f.isDirectory()){continue;}
-            ret.add(f);
+            if (f.getName().contains("cat")) {catFiels.add(f);}
+            else {dogFiels.add(f);}
         }
-        return ret;
     }
 
-    static ImageDataSetIterator setImagesTrain(ImageDataSetIterator input, int iteration) {
+    static ImageDataSetIterator setImagesTrain(ImageDataSetIterator input, int begin, int amont) {
         ArrayList<String> paths = new ArrayList<>();
         ArrayList<double[]> outs = new ArrayList<>();
-        for (int i = picturesPerIteration*iteration; i < picturesPerIteration*(iteration+1); i++) {
-            paths.add(carFiles.get(i).getAbsolutePath());
-            outs.add(new double[]{correctValue, wrongValue, wrongValue});
+        for (int i = begin; i < amont; i++) {
+            paths.add(catFiels.get(i).getAbsolutePath());
+            outs.add(new double[]{correctValue, wrongValue});
         }
         input.addDataString((List<String>) paths.clone(), (List<double[]>) outs.clone());
         paths = new ArrayList<>(); outs = new ArrayList<>();
-        for (int i = picturesPerIteration*iteration; i < picturesPerIteration*(iteration+1); i++) {
-            paths.add(flowerFiles.get(i).getAbsolutePath());
-            outs.add(new double[]{wrongValue, correctValue, wrongValue});
-        }
-        input.addDataString((List<String>) paths.clone(), (List<double[]>) outs.clone());
-        paths = new ArrayList<>(); outs = new ArrayList<>();
-        for (int i = picturesPerIteration*iteration; i < picturesPerIteration*(iteration+1); i++) {
-            paths.add(airplaneFiles.get(i).getAbsolutePath());
-            outs.add(new double[]{wrongValue, wrongValue, correctValue});
+        for (int i = begin; i < amont; i++) {
+            paths.add(dogFiels.get(i).getAbsolutePath());
+            outs.add(new double[]{wrongValue, correctValue});
         }
         input.addDataString((List<String>) paths.clone(), (List<double[]>) outs.clone());
         return input;
     }
 
-
-    static ImageDataSetIterator setImagesTest(ImageDataSetIterator input) {
-        ArrayList<String> paths = new ArrayList<>();
-        ArrayList<double[]> outs = new ArrayList<>();
-        for (int i = car_correct; i < carFiles.size(); i++) {
-            paths.add(carFiles.get(i).getAbsolutePath());
-            outs.add(new double[]{correctValue, wrongValue, wrongValue});
-        }
-        input.addDataString((List<String>) paths.clone(), (List<double[]>) outs.clone());
-        paths = new ArrayList<>(); outs = new ArrayList<>();
-        for (int i = flower_correct; i < flowerFiles.size(); i++) {
-            paths.add(flowerFiles.get(i).getAbsolutePath());
-            outs.add(new double[]{wrongValue, correctValue, wrongValue});
-        }
-        input.addDataString((List<String>) paths.clone(), (List<double[]>) outs.clone());
-        paths = new ArrayList<>(); outs = new ArrayList<>();
-        for (int i = airplane_correct; i < airplaneFiles.size(); i++) {
-            paths.add(airplaneFiles.get(i).getAbsolutePath());
-            outs.add(new double[]{wrongValue, wrongValue, correctValue});
-        }
-        input.addDataString((List<String>) paths.clone(), (List<double[]>) outs.clone());
-        return input;
-    }
 }
