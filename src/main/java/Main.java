739315b76d69847c3c50317929a596f90120b885
Rubilia <Rubiliam2003@gmail.com>
2018-10-14 22:48:50 +0300

    Performance comparison of gpu and cpu learning

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,57 +20,59 @@ import java.util.Random;
  */
 
 public class Main {
-    private static int picturesPerIteration = 900;
+    private static int picturesPerIteration = 500;
     private static final double correctValue = 0.9, wrongValue = 0.1;
     static List<File> catFiels, dogFiels;
     private static final Logger log = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
         System.setProperty("org.bytedeco.javacpp.maxphysicalbytes", "0");
         System.setProperty("org.bytedeco.javacpp.maxbytes", "0");
+        for (int i = 0; i < 10; i++) {
+            run();
+            catFiels.clear();
+            dogFiels.clear();
+        }
+        long s = 0;
+        for (long i : times){
+            s+=i;
+        }
+        System.out.println("avg time: " + s/times.size());
+    }
+    static ArrayList<Long> times =new ArrayList<>();
+    public static void run() {
+        long time = System.currentTimeMillis();
         CudaEnvironment.getInstance().getConfiguration().allowMultiGPU(true).setMaximumDeviceCache(2L * 1024L * 1024L * 1024L).allowCrossDeviceAccess(true);
         getFiles("C:\\Users\\Rubil\\Documents\\NeuralNetworkProjects\\Cat_Dog\\train\\");
         int nChannels = 3;
         int outputNum = 2;
         int batchSize = 3;
-        int nEpochs = 100;
+        int nEpochs = 1;
         int inputHeight = 160, inputWidth  = 160;
         int seed = (new Random()).nextInt();
-        log.info("Building a model....");
         ZooModel inception = InceptionResNetV1.builder().numClasses(2).seed(seed).build();
         inception.setInputShape(new int[][]{{nChannels, inputHeight, inputWidth}, {batchSize}});
         ComputationGraph model = inception.init();
-        model.setListeners(new PerformanceListener(1, true));
-        log.info("Loading training data....");
         DataSetIterator Train;
-        System.out.println("Training model....");
-        long timeX = System.currentTimeMillis();
         for( int i=0; i<nEpochs; i++ ) {
-            System.out.println("new Epoch " + i);
-            long time1 = System.currentTimeMillis();
             ParallelWrapper wrapper = new ParallelWrapper.Builder(model)
                     .prefetchBuffer(batchSize)
                     .workers(4)
                     .averagingFrequency(2)
-                    .reportScoreAfterAveraging(true)
+                    .reportScoreAfterAveraging(false)
                     .build();
             for (int j = 0; j < nEpochs; j++) {
-                for (int k = 0; k < (catFiels.size()+dogFiels.size())/picturesPerIteration-1; k++) {
+                for (int k = 0; k < 1; k++) {
                     Train = setImagesTrain(new ImageDataSetIterator(batchSize, inputHeight, inputWidth, outputNum), 0, picturesPerIteration);
                     wrapper.fit(Train);
                     Train.reset();
                 }
             }
-            long time2 = System.currentTimeMillis();
-            System.out.printf("*** Completed epoch {}, time: {} ***", i, (time2 - time1)/1000 + " sec");
         }
-        long timeY = System.currentTimeMillis();
-        System.out.printf("*** Training complete, time: {} ***", (timeY - timeX));
-        log.info("Getting data for test");
-        DataSetIterator Test = setImagesTrain(new ImageDataSetIterator(batchSize, inputHeight, inputWidth, outputNum), 10000, 10500);
-        log.info("Evaluate model....");
+        DataSetIterator Test = setImagesTrain(new ImageDataSetIterator(batchSize, inputHeight, inputWidth, outputNum), 10000, 11000);
         Evaluation eval = model.evaluate(Test);
-        log.info(eval.stats());
-        System.out.println("****************Example finished********************");
+        long endTime = System.currentTimeMillis()-time;
+        System.out.println("total time: " + endTime);
+        times.add(endTime);
     }
 
     static void getFiles(String path){
